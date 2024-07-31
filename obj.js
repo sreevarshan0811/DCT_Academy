1c387c834d8fc36d3b64db4724193b64ffe22bf0
Sree Varshan.T <109500445+SreevarshanURK20CS1139@users.noreply.github.com>
2024-07-31 11:01:59 +0530

    initial commit

--- obj.js
+++ obj.js
@@ -1,6 +1,6 @@
 // git init
 // git add README.md
-// git commit -m "initial commit"
+// git commit -m "first commit"
 // git branch -M main
 // git remote add origin https://github.com/sreevarshan0811/exe_1.git
 // git push -u origin main
