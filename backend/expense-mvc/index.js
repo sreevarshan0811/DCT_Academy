f6c5cfdfd331e5fc8b7f24aa86cf3dd99ad17669
Sree Varshan.T <109500445+SreevarshanURK20CS1139@users.noreply.github.com>
2024-09-10 21:35:51 +0530

    Expense_App_MVC

---
+++ backend/expense-mvc/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3030
+const configureDb = require('./config/db')
+const {checkSchema , validationResult} = require('express-validator')
+const idValidationSchema = require('./app/validators/id-schema-validation')
+const categoriesCltr = require('./app/controllers/categories-cltr')
+const expensesCltr = require('./app/controllers/expenses-cltr')
+configureDb()
+app.use(express.json())
+
+
+//Categories API
+
+app.get('/categories', categoriesCltr.list) // --->  all categories
+
+app.get('/categories-only-names', categoriesCltr.names) // ---> only category names
+
+app.get('/categories/:id',checkSchema(idValidationSchema),categoriesCltr.show) // ---> single category by id
+
+app.post('/categories', categoriesCltr.create) // ---> create a new category
+
+app.put('/categories/:id',categoriesCltr.update) // ---> update a category
+
+app.delete('/categories/:id' ,checkSchema(idValidationSchema), categoriesCltr.delete) // ---> delete a category
+
+
+
+//Expenses API
+
+app.get('/expenses', expensesCltr.list)// ---> all expenses
+
+app.get('/expenses/:id', expensesCltr.show) // ---> single expense
+ 
+app.get('/expenses-total', expensesCltr.total) // ---> total expense
+
+app.post('/expenses', expensesCltr.create) // ---> create new expense
+
+app.put('/expenses/:id', expensesCltr.update) // ---> update an expense
+
+app.delete('/expenses/:id', expensesCltr.delete) // ---> delete an expense
+
+
+app.listen(port, () => {
+    console.log('express server is running on port', port)
+})
